3c882c3a29b8b91844f58175aacae3c1dc2e55df
nagabharathnr <nagnr1994@gmail.com>
2024-04-24 12:31:28 +0100

    adding git demo proj

---
+++ GitJenkinsDemo.py
@@ -0,0 +1,23 @@
+#def print_hi(name):
+#    print(name)
+import os
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, IntegerType, StringType
+
+if __name__ == '__main__':
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    spark = SparkSession.builder.master("local").appName("p2").getOrCreate()
+
+    orderSchema = StructType([
+        StructField("orderid", IntegerType(), nullable=True),
+        StructField("orderdate", StringType(), nullable=True),
+        StructField("custid", IntegerType(), nullable=True),
+        StructField("orderstatus", StringType(), nullable=True)
+    ])
+    #ordersdf = spark.read.option("header","true").schema(orderSchema).csv("C:\\Users\\cheru\\PycharmProjects\\pythonProject1\\orders.csv")
+    ordersdf = spark.read.option("header",True).schema(orderSchema).csv("/user/ec2-user/UKUSMarHDFS/bharathi/orders.csv")
+    print("testing webhook")
+    #print("testing 2 webhook")
+    ordersdf.show(5)
