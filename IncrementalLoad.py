e621dee4c5b8dca8dd52c16696cf92db03786e7a
nagabharathnr <nagnr1994@gmail.com>
2024-05-01 12:18:29 +0100

    changed output path

--- IncrementalLoad.py
+++ IncrementalLoad.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
 
     new_batting_df.show()
     new_summary_df.show()
-    print("incremental laod - jenkins setup2")
+    print("incremental laod - jenkins setup3")
 
 
 
